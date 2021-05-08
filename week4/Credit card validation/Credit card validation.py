ee1b07c3a13d55ee8c84c62f62e67f8df0732335
Maksim Petev <petevmv@gmail.com>
2021-05-08 03:54:08 +0300

    Credit card task solution

---
+++ week4/Credit card validation/Credit card validation.py
@@ -0,0 +1,23 @@
+def sum_of_digits(x):
+    if x * 2 >= 10:
+        y = str(x * 2)
+        return (int(y[0]) + int(y[1]))
+    else:
+        return x * 2
+def is_credit_card_valid(number):
+    num_list = []
+    for idx, num in enumerate(reversed(str(number))):
+        if idx % 2 != 0:
+            num_list.append(sum_of_digits(int(num)))
+        else:
+            num_list.append(int(num))
+    result = sum(num_list)
+    if result % 10 == 0:
+        print("valid")
+        return result
+    else:
+        print("not valid")
+        return result
+
+
+print(is_credit_card_valid(4417123456789113))
