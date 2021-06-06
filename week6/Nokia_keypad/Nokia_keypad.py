cee3f0a536dc2c085318d2ddaf773895a6537ac7
Maksim Petev <petevmv@gmail.com>
2021-06-06 10:55:33 +0300

    Nokia keypad - message to numbers solution

--- week6/Nokia_keypad/Nokia_keypad.py
+++ week6/Nokia_keypad/Nokia_keypad.py
@@ -32,23 +32,50 @@ def numbers_to_message(pressed_sequence):
     return ''.join(letters)
 
 
-print(numbers_to_message([0, 0, 0, 0]) == '    ')
-numbers_to_message([2, -1, 2, 2, -1, 2, 2, 2])
-numbers_to_message([2])
-numbers_to_message([1, 4, 4, 4, 8, 8, 8, 6, 6, 6, 0, 3, 3, 0, 1, 7, 2, 6, 6, 3, 2])
-numbers_to_message([2, -1, 2, -1, 2, 2, -1, 2, 2, -1, 2, 2, 2, -1, 2, 2, 2])
+# print(numbers_to_message([0, 0, 0, 0]) == '    ')
+# numbers_to_message([2, -1, 2, 2, -1, 2, 2, 2])
+# numbers_to_message([2])
+# numbers_to_message([1, 4, 4, 4, 8, 8, 8, 6, 6, 6, 0, 3, 3, 0, 1, 7, 2, 6, 6, 3, 2])
+# numbers_to_message([2, -1, 2, -1, 2, 2, -1, 2, 2, -1, 2, 2, 2, -1, 2, 2, 2])
 
 
 
 def message_to_numbers(message):
-    pass
+    result = []
+    nums = []
+    keypad = {0:" ",2:'abc',
+              3:'def', 4:'ghi',
+              5:'jkl',
+              6:"mno", 7:"pqrs",
+              8:"tuv", 9:"wxyz"}
+
+    for m in message:
+        if m.isupper():
+            nums.append([1])
+        for k, v in keypad.items():
+            for letter in v:
+                if letter == m.lower():
+                    times_pressed = v.index(letter) + 1
+                    nums.append([k]*times_pressed)
+
+    for idx, val in enumerate(nums[:-1]):
+        if val[0] == nums[idx-1][0]:
+            val.append(-1)
+
+    for i in nums:
+         result.extend(i)
+
+    print(result)
+    return result
 
 
-# message_to_numbers("abc")
+
+
+#message_to_numbers("abc")
 #[2, -1, 2, 2, -1, 2, 2, 2]
-# message_to_numbers("a")
+#message_to_numbers("a")
 # [2]
-# message_to_numbers("Ivo e Panda")
+message_to_numbers("Ivo e Panda")
 # [1, 4, 4, 4, 8, 8, 8, 6, 6, 6, 0, 3, 3, 0, 1, 7, 2, 6, 6, 3, 2]
-# message_to_numbers("aabbcc")
+#message_to_numbers("aabbcc")
 # [2, -1, 2, -1, 2, 2, -1, 2, 2, -1, 2, 2, 2, -1, 2, 2, 2]
