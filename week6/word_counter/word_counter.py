cf01287764d8c5af4a2eca2d2d7a70191c49d22f
Maksim Petev <petevmv@gmail.com>
2021-07-13 00:49:54 +0300

    removing commented codeblocks

--- week6/word_counter/word_counter.py
+++ week6/word_counter/word_counter.py
@@ -1,4 +1,5 @@
 import numpy as np
+
 def to_string(alist):
     str = ''
     return (str.join(alist))
@@ -12,63 +13,72 @@ def word_counter(matrix, word):
     diags.extend(matrix.diagonal(i) for i in range(matrix.shape[1]-1,-matrix.shape[0],-1))
 
     diagonals = [n.tolist() for n in diags]
+
     # diagonals left to right and right to left
     for i in diagonals:
         if word in to_string(i):
             result += 1
         if word in to_string(list(reversed(i))):
             result += 1
-        # if word in to_string(list(reversed(i))):
-        #     print(list(reversed(i)))
-
-    # result += diagonals.count(list(word))
-
-    # diagonals right to left
-    # result += diagonals.count(list(reversed(word)))
 
     # rows left to right and right to left
-    # result += matrix.tolist().count(list(word))
     for i in matrix.tolist():
         if word in to_string(i):
             result += 1
         if word in to_string(list(reversed(i))):
             result += 1
 
-    # rows right to left
-    # result += matrix.tolist().count((list(reversed(word))))
-    #print(result)
-
-    # columns
-    # a = matrix[:,2]
-    # print(list(reversed(a.tolist()))[0:len(word)])
+    # columns top to bottom and bottom to top
     for i in range((len(matrix[0,:]))):
         if word in to_string(matrix[::,i].tolist()):
             result += 1
         if word in to_string(list(reversed(matrix[::,i].tolist()))):
             result += 1
-
-
-
-    # for row in matrix:
-    #     if list(word) == row:
-    #         print(True)
-
-
-    # for row_index in range(len(matrix)):
-    #     for col_index in range(len(matrix[0])):
+    # anagram check
     if word == word[::-1]:
         result = result//2
 
 
     print(result)
     return result
-
-to_string(["z", "v", "a", "n", "q", "h", "r", "e", "z", "g", "t", "z"])
-word = "python"
+word = "ivan"
 matrix = [
-  ["r", "u", "b", "y"],
-  ["r", "u", "b", "y"],
-  ["r", "u", "b", "y"],
-  ["r", "u", "b", "y"],
+    ["i", "v", "a", "n"],
+    ["e", "v", "n", "h"],
+    ["i", "n", "a", "v"],
+    ["m", "v", "v", "n"],
+    ["q", "r", "i", "t"]
 ]
+#
+# word = "actually"
+# matrix = [
+#     ["i", "v", "a", "n", "q", "h", "r", "e", "z", "g", "t", "z", "o", "y", "m"],
+#     ["e", "v", "n", "h", "t", "r", "x", "e", "k", "y", "d", "a", "i", "l", "c"],
+#     ["i", "a", "c", "t", "u", "a", "l", "l", "y", "m", "c", "x", "r", "l", "e"],
+#     ["m", "v", "c", "n", "p", "u", "a", "m", "n", "t", "l", "u", "e", "a", "a"],
+#     ["q", "r", "i", "t", "w", "e", "a", "q", "u", "p", "r", "x", "t", "u", "z"],
+#     ["p", "e", "a", "c", "t", "u", "a", "l", "l", "y", "w", "p", "y", "t", "m"],
+#     ["o", "y", "h", "t", "r", "e", "l", "u", "f", "p", "q", "n", "z", "c", "s"],
+#     ["p", "a", "c", "t", "u", "a", "l", "l", "y", "u", "r", "e", "q", "a", "r"]
+# ]
+#
+# word = "madam"
+# matrix = [
+#     ["z", "v", "a", "n", "q", "h", "r", "e", "z", "g", "t", "z"],
+#     ["e", "v", "m", "h", "t", "r", "x", "e", "k", "y", "m", "a"],
+#     ["i", "a", "c", "a", "u", "a", "l", "l", "y", "a", "c", "x"],
+#     ["m", "v", "c", "n", "d", "u", "a", "m", "d", "t", "l", "u"],
+#     ["q", "t", "i", "t", "w", "a", "a", "a", "u", "p", "r", "x"],
+#     ["p", "e", "m", "a", "d", "a", "m", "l", "l", "y", "w", "p"],
+#     ["o", "y", "h", "t", "e", "e", "l", "u", "f", "p", "q", "n"],
+#     ["p", "a", "c", "t", "u", "a", "l", "l", "y", "u", "r", "e"]
+# ]
+#
+# word = "python"
+# matrix = [
+#   ["r", "u", "b", "y"],
+#   ["r", "u", "b", "y"],
+#   ["r", "u", "b", "y"],
+#   ["r", "u", "b", "y"],
+# ]
 word_counter(matrix, word)
