d0cc22cf805fc3835896565166bdf5c737947901
Maksim Petev <petevmv@gmail.com>
2021-07-19 13:04:11 +0300

    Function now handle first column

--- week6/Matrix_bomber/matrix_bomber.py
+++ week6/Matrix_bomber/matrix_bomber.py
@@ -9,26 +9,32 @@ def matrix_bombing_plan(m):
         for j in range(m):
             # print(matrix[i][j], i, j)
             matrix_copy = matrix.copy()
+
+            # Upper left corner
             if (i, j) == (0, 0):
                 matrix_copy[0][1] = matrix[0][1] - matrix[0][0]
                 matrix_copy[1][0] = matrix[1][0] - matrix[0][0]
                 matrix_copy[1][1] = matrix[1][1] - matrix[0][0]
 
+            # Lower left corner
             if (i, j) == (last_row, 0):
                 matrix_copy[last_row][1] = matrix[last_row][1] - matrix[last_row][0]
                 matrix_copy[last_row-1][1] = matrix[last_row-1][1] - matrix[last_row][0]
                 matrix_copy[last_row-1][0] = matrix[last_row-1][0] - matrix[last_row][0]
 
+            # Upper right corner
             if (i, j) == (0,last_col):
                 matrix_copy[0][last_col-1] = matrix[0][last_col-1] - matrix[0][last_col]
                 matrix_copy[1][last_col-1] = matrix[1][last_col-1] - matrix[0][last_col]
                 matrix_copy[1][last_col] = matrix[1][last_col] - matrix[0][last_col]
 
+            # Lower right corner
             if (i, j) == (last_row,last_col):
                 matrix_copy[last_row][last_col-1] = matrix[last_row][last_col-1] - matrix[last_row][last_col]
                 matrix_copy[last_row-1][last_col-1] = matrix[last_row-1][last_col-1] - matrix[last_row][last_col]
                 matrix_copy[last_row-1][last_col] = matrix[last_row-1][last_col] - matrix[last_row][last_col]
 
+            # First row
             if (i, j) == (0, j) and (i, j) != (0,0) and (i, j) != (0, last_col):
                 matrix_copy[i][j-1] = matrix[i][j-1] - matrix[i][j]
                 matrix_copy[i][j+1] = matrix[i][j+1] - matrix[i][j]
@@ -36,13 +42,22 @@ def matrix_bombing_plan(m):
                 matrix_copy[i+1][j-1] = matrix[i+1][j-1] - matrix[i][j]
                 matrix_copy[i+1][j+1] = matrix[i+1][j+1] - matrix[i][j]
 
+            # Last row
             if (i, j) == (last_row, j) and (i, j) != (last_row,0) and (i, j) != (last_row, last_col):
-                    print(i,j)
                     matrix_copy[i][j-1] = matrix[i][j-1] - matrix[i][j]
                     matrix_copy[i][j+1] = matrix[i][j+1] - matrix[i][j]
                     matrix_copy[i-1][j] = matrix[i-1][j] - matrix[i][j]
                     matrix_copy[i-1][j+1] = matrix[i-1][j+1] - matrix[i][j]
                     matrix_copy[i-1][j-1] = matrix[i-1][j-1] - matrix[i][j]
+
+            # First col
+            if (i, j) == (i, 0) and (i, j) != (0, 0) and (i, j) != (last_row, 0):
+                print(i,j)
+                matrix_copy[i][j+1] = matrix[i][j+1] - matrix[i][j]
+                matrix_copy[i-1][j+1] = matrix[i-1][j+1] - matrix[i][j]
+                matrix_copy[i-1][j] = matrix[i-1][j] - matrix[i][j]
+                matrix_copy[i+1][j] = matrix[i+1][j] - matrix[i][j]
+                matrix_copy[i+1][j+1] = matrix[i+1][j+1] - matrix[i][j]
 
 
             matrix_copy[matrix_copy < 0] = 0
