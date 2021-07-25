a1205f618654d81353fdd52352b7f9d4d2c1e3b1
Maksim Petev <petevmv@gmail.com>
2021-07-25 23:54:44 +0300

    was not able to fully implement it so watched the video solution for inside

--- concept2_linux_and_python_setup/reduce_file_path.py
+++ concept2_linux_and_python_setup/reduce_file_path.py
@@ -1,2 +1,28 @@
+from collections import deque
+
 def reduce_file_path(path):
-    pass
+    result = deque()
+    for ch in path.split('/'):
+        if ch == '..' and len(result) > 0:
+            result.pop()
+        if ch not in ('.','..',''):
+            result.append(ch)
+
+
+    print('/' + '/'.join(result))
+    return '/' + '/'.join(result)
+
+
+
+
+
+print(reduce_file_path("/") == "/")
+print(reduce_file_path("/srv/../") == "/")
+print(reduce_file_path("/srv/www/htdocs/wtf/") == "/srv/www/htdocs/wtf")
+print(reduce_file_path("/srv/www/htdocs/wtf") == "/srv/www/htdocs/wtf")
+print(reduce_file_path("/srv/./././././") == "/srv")
+print(reduce_file_path("/etc//wtf/") == "/etc/wtf")
+print(reduce_file_path("/etc/../etc/../etc/../") == "/")
+print(reduce_file_path("//////////////") == "/")
+print(reduce_file_path("/../") == "/")
+print(reduce_file_path('/home//user/courses/./Programming101-Python/week01/../') == '/home/user/courses/Programming101-Python')
