4046f112f403942149ada9b257ff063631cba749
Maksim Petev <petevmv@gmail.com>
2021-08-07 08:47:52 -0400

    Concept3 first commit

---
+++ Concept3_Unit_Testing/interval_extended.py
@@ -0,0 +1,50 @@
+class Interval:
+    def __init__(self, start, end, start_opened=False, end_opened=False):
+        # This is the constructor
+        pass
+
+    def is_inside(self, value):
+        pass
+
+    def stringify(self):
+        pass
+
+
+
+
+
+closed_interval = Interval(1, 10)
+
+closed_interval.is_inside(1) is True
+closed_interval.is_inside(5) is True
+closed_interval.is_inside(10) is True
+
+closed_interval.stringify() == "[1, 10]"
+
+
+opened_interval = Interval(1, 10, start_opened=True, end_opened=True)
+
+opened_interval.is_inside(1) is False
+opened_interval.is_inside(5) is True
+opened_interval.is_inside(10) is False
+
+opened_interval.stringify() == "(1, 10)"
+
+
+half_opened_interval = Interval(1, 10, start_opened=False, end_opened=True)
+
+half_opened_interval.is_inside(1) is True
+half_opened_interval.is_inside(5) is True
+half_opened_interval.is_inside(10) is False
+
+half_opened_interval.stringify() == "[1, 10)"
+
+
+
+half_opened_interval = Interval(1, 10, start_opened=True, end_opened=False)
+
+half_opened_interval.is_inside(1) is False
+half_opened_interval.is_inside(5) is True
+half_opened_interval.is_inside(10) is True
+
+half_opened_interval.stringify() == "(1, 10]"
