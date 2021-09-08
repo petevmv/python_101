13aaaca1c100519bbf1272bcb00d56688e7eb84c
Maksim Petev <petevmv@gmail.com>
2021-09-08 14:12:48 -0400

    add some partile working code

--- Concept3_Unit_Testing/Simple_template_engine/template.py
+++ Concept3_Unit_Testing/Simple_template_engine/template.py
@@ -1,6 +1,7 @@
 class TemplateEngine:
     def __init__(self, template):
-        pass
+        self.template = template
+
 
     def render(self, **context):
         """
@@ -8,8 +9,31 @@ class TemplateEngine:
 
         Raise `TemplatEngineError` if not all variables, present in `self.template`, have values in `context`.
         """
+        self.context = context
+        bracket_location = self.template.find('{')
+        greet = template[:bracket_location]
+        list_of_args = [x for x in template[bracket_location:] if x not in ('{', '}',' ')]
+        return f"{greet}{self.context[list_of_args[0]]}"
 
     def extract_variables(self):
         """
         Returns a list of all variables names, that are present in `self.template`
         """
+        keys = [k for k,v in self.context.items()]
+        return f"{keys}"
+
+template = """
+Hello {{ first_name }} {{ last_name }},
+
+I hope this email finds you well.
+
+We are currently running a promotion for {{ product }}.
+
+You can get your discount {{ here }}
+"""
+
+engine = TemplateEngine(template)
+rendered = engine.render(x='General Kenobi.')
+print(rendered)
+var = engine.extract_variables()
+print(var)
