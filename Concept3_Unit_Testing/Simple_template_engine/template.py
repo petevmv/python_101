9ddd88fd232821317ea1dd273f84900c8d2821a9
Maksim Petev <petevmv@gmail.com>
2021-09-08 21:26:01 -0400

    add logic to interpolate kwargs

--- Concept3_Unit_Testing/Simple_template_engine/template.py
+++ Concept3_Unit_Testing/Simple_template_engine/template.py
@@ -1,3 +1,4 @@
+import re
 class TemplateEngine:
     def __init__(self, template):
         self.template = template
@@ -10,17 +11,23 @@ class TemplateEngine:
         Raise `TemplatEngineError` if not all variables, present in `self.template`, have values in `context`.
         """
         self.context = context
-        bracket_location = self.template.find('{')
-        greet = template[:bracket_location]
-        list_of_args = [x for x in template[bracket_location:] if x not in ('{', '}',' ')]
-        return f"{greet}{self.context[list_of_args[0]]}"
-
+        pattern = r'({*{[ A-Za-z0-9_ ]+}*.})'
+        found_result = re.findall(pattern, self.template)
+        if len(found_result) != len(self.context.keys()):
+            raise TemplatEngineError
+        text = self.template
+        values = [v for k,v in self.context.items()]        
+        for idx, el in enumerate(found_result):
+            text = text.replace(el, values[idx])
+        return text
+    
     def extract_variables(self):
         """
         Returns a list of all variables names, that are present in `self.template`
         """
         keys = [k for k,v in self.context.items()]
         return f"{keys}"
+
 
 template = """
 Hello {{ first_name }} {{ last_name }},
@@ -33,7 +40,8 @@ You can get your discount {{ here }}
 """
 
 engine = TemplateEngine(template)
-rendered = engine.render(x='General Kenobi.')
+rendered = engine.render(first_name='Ivan',
+    last_name='Ivanov', product='Python course', here='some_link')
 print(rendered)
 var = engine.extract_variables()
 print(var)
