a6251dc922ca8ecf8672929728fd9513750ad3ff
Maksim Petev <petevmv@gmail.com>
2021-10-10 11:29:26 -0400

    core logic run as intendet, added a .gif animation

--- Concept3_Unit_Testing/Conways_game_of_life/game_of_life.py
+++ Concept3_Unit_Testing/Conways_game_of_life/game_of_life.py
@@ -1,8 +1,12 @@
 import numpy as np
+from PIL import Image as im
+import matplotlib.pyplot as plt
+import matplotlib.animation as animation
+import ipdb
 
 def update_grid(idx_i, idx_j, number_of_live_neighbors, matrix):
 	matrix_copy = matrix.copy()
-	print(number_of_live_neighbors)
+	# print(number_of_live_neighbors)
 
 	if number_of_live_neighbors in [2, 3] and matrix[idx_i][idx_j] == 1:
 		matrix_copy[idx_i][idx_j] = 1
@@ -12,7 +16,7 @@ def update_grid(idx_i, idx_j, number_of_live_neighbors, matrix):
 		matrix_copy[idx_i][idx_j] = 0
 	return matrix_copy
 
-def get_neighbor(matrix):
+def run(matrix):
 	size = matrix.shape
 	matrix_copy = matrix.copy()
 	number_of_live_neighbors = 0
@@ -126,7 +130,7 @@ def get_neighbor(matrix):
 
 			# last col
 			if (idx_i, idx_j) == (idx_i, last_col) and idx_i != 0 and idx_i != last_row:
-				if matrix[idx_i- 1][idx_j] == 1:
+				if matrix[idx_i - 1][idx_j] == 1:
 					number_of_live_neighbors += 1
 				if matrix[idx_i - 1][idx_j - 1] == 1:
 					number_of_live_neighbors += 1
@@ -162,20 +166,66 @@ def get_neighbor(matrix):
 				matrix_copy = update_grid(idx_i, idx_j, number_of_live_neighbors, matrix_copy.copy())
 				number_of_live_neighbors = 0
 				continue
+	# print(matrix_copy)
+	return matrix_copy
 
 
 
 
   
-	print(matrix_copy)
+	
 	# print(number_of_live_neighbors)
 			
 	
 
+universe = np.zeros((50, 50))
+# test = np.array([[0, 1, 0],[0,1,1],[1,0,1],[1,0,1]], dtype=np.uint8)
+beacon = [[1, 1, 0, 0],
+		  [1, 1, 0, 0],
+		  [0, 0, 1, 1],
+		  [0, 0, 1, 1]]
 
-test = np.array([[0, 1, 0],[0,1,1],[1,0,1],[1,0,1]])
-a = np.random.randint(2, size=(3,3))
+R_pentomino = [[0, 0, 0, 0, 0],
+			   [0, 0, 1, 1, 0],
+			   [0, 1, 1, 0, 0],
+			   [0, 0, 1, 0, 0],
+			   [0, 0, 0, 0, 0]]
+
+acron = [[1,1,0,1,1,1],
+		 [0,0,0,1,0,0],
+		 [0,1,0,0,0,0]]
+
+glider =[[0,0,1],
+		 [1,0,1],
+		 [0,1,1]]
+		 
+		 
+# a = np.random.randint(2, size=(4,4), dtype=np.uint8)
 # print(a)
 # print("______________________")
 # print(test[0])
-get_neighbor(test)
+
+# universe[15:20, 21:26] = R_pentomino
+# universe[0:3, 0:3] = glider
+# universe[1:6, 1:7] = glider
+universe[30: 33,30:36] = acron
+# print(test)
+
+
+fig = plt.figure()
+plt.axis('off')
+images = []
+
+
+
+if __name__ == "__main__":
+	for i in range(30):
+		# ipdb.set_trace()
+		images.append((plt.imshow(universe, cmap='binary'),))
+		universe = run(universe)
+		# plt.show()
+
+
+	im_animation = animation.ArtistAnimation(fig, images, interval=100, repeat_delay=1000, blit=True)
+	plt.show()
+	im_animation.save('ani.gif', writer='imagemagic')
---
+++ Concept3_Unit_Testing/Conways_game_of_life/test_game_of_life.py
@@ -0,0 +1,7 @@
+import game_of_life
+import unittest
+
+def test_game_of_life():
+		
+
+
