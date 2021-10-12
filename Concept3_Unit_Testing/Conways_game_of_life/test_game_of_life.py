63d86eefcf17fa31663ac89a4e9d7648fbab8f9e
Maksim Petev <petevmv@gmail.com>
2021-10-12 15:05:15 -0400

    update_grid test

--- Concept3_Unit_Testing/Conways_game_of_life/test_game_of_life.py
+++ Concept3_Unit_Testing/Conways_game_of_life/test_game_of_life.py
@@ -5,15 +5,17 @@ import ipdb
 
 class Test_game_of_life(unittest.TestCase):
 	def test_run(self):
-		initial_input = np.array([[0, 1, 0],
-					 [0, 1, 1],
-					 [1, 0, 1],
-					 [1, 0, 1]])
+		initial_input = np.array(
+			[[0, 1, 0],
+			 [0, 1, 1],
+			 [1, 0, 1],
+			 [1, 0, 1]])
 		  	
-		expected = np.array([[0, 1, 1],
-					[1, 0, 1],
-					[1, 0, 1],
-					[0, 0, 0]])
+		expected = np.array(
+			[[0, 1, 1],
+			 [1, 0, 1],
+			 [1, 0, 1],
+			 [0, 0, 0]])
 
 		self.assertEqual(game_of_life.run(initial_input).tolist(), expected.tolist())
 
@@ -23,19 +25,21 @@ class Test_game_of_life(unittest.TestCase):
 		seed = game_of_life.glider
 		board[1:4, 1:4] = seed
 		
-		expected = np.array([[0, 0, 0, 0, 0 ,0],
-							 [0, 0, 0, 1, 0, 0],
-							 [0, 1, 0, 1, 0, 0],
-							 [0, 0, 1, 1, 0, 0],
-							 [0, 0, 0, 0, 0, 0],
-							 [0, 0, 0, 0, 0, 0]])
-		
-		expected_after_one_gen = np.array([[0, 0, 0, 0, 0, 0],
-									   	   [0, 0, 1, 0, 0, 0],
-										   [0, 0, 0, 1, 1, 0],
-										   [0, 0, 1, 1, 0, 0],
-										   [0, 0, 0, 0, 0, 0],
-										   [0, 0, 0, 0, 0, 0]])
+		expected = np.array(
+			[[0, 0, 0, 0, 0 ,0],							 
+			 [0, 0, 0, 1, 0, 0],
+			 [0, 1, 0, 1, 0, 0],
+			 [0, 0, 1, 1, 0, 0],
+			 [0, 0, 0, 0, 0, 0],
+			 [0, 0, 0, 0, 0, 0]])
+
+		expected_after_one_gen = np.array(
+			[[0, 0, 0, 0, 0, 0],
+			 [0, 0, 1, 0, 0, 0],
+			 [0, 0, 0, 1, 1, 0],
+			 [0, 0, 1, 1, 0, 0],
+			 [0, 0, 0, 0, 0, 0],
+			 [0, 0, 0, 0, 0, 0]])
 
 		
 		self.assertEqual(board.tolist(), expected.tolist())
@@ -43,11 +47,12 @@ class Test_game_of_life(unittest.TestCase):
 							 expected_after_one_gen.tolist())
 	
 	def test_update_grid(self):
-		initial_input = np.array([[0, 1, 0],
-								  [0, 1, 1],
-								  [1, 0, 1],
-								  [1, 0, 1]])
-		
+		initial_input = np.array(
+			[[0, 1, 0],
+			 [0, 1, 1],
+			 [1, 0, 1],
+			 [1, 0, 1]])
+
 		spawn_live_cell_on_i_j = game_of_life.update_grid(0, 2, 3, initial_input)[0][2]
 		kill_cell_on_i_j = game_of_life.update_grid(3, 0, 1, initial_input)[3][0]
 		survival_cell_on_i_j = game_of_life.update_grid(0, 1, 2, initial_input)[0][1]
