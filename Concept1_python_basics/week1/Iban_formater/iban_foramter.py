bcc38495b725af6e823c87fa215ab02cb21f8b2a
Maksim Petev <petevmv@gmail.com>
2022-06-10 15:28:13 -0400

    small refactor

--- Concept1_python_basics/week1/Iban_formater/iban_foramter.py
+++ Concept1_python_basics/week1/Iban_formater/iban_foramter.py
@@ -1,5 +1,11 @@
 def iban_formater(iban):
-    for i in iban:
-        if i == ' ':
-            return iban
-    return (iban[:4] + " " + iban[4:8] + " " + iban[8:12] + " " + iban[12:16] + " " + iban[16:20] + " " + iban[20:])
+    result = []
+    iban = iban.replace(' ', '')
+    for idx, num in enumerate(iban):
+        if (idx) % 4 == 0:
+            result.append(' ')
+        
+        result.append(num)
+    return ''.join(result).lstrip()
+    
+
