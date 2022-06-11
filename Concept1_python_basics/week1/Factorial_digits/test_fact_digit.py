eca381725035eb5f4b6b8da7bb79c581d05ed584
Maksim Petev <petevmv@gmail.com>
2022-06-11 17:00:49 -0400

    add tests for fact_digits()

---
+++ Concept1_python_basics/week1/Factorial_digits/test_fact_digit.py
@@ -0,0 +1,30 @@
+import unittest
+from fact_digit import fact_digits
+
+class TestFactorialDigit(unittest.TestCase):
+	def test_fact_digit(self):
+		with self.subTest('Value Error'):
+			with self.assertRaises(ValueError):
+				fact_digits('abc')
+			with self.assertRaises(ValueError):
+				fact_digits([])
+			with self.assertRaises(ValueError):
+				fact_digits({})
+			with self.assertRaises(ValueError):
+				fact_digits((1, 2))
+
+		with self.subTest('Testing fact_digit() behave as expected'):
+			self.assertEqual(fact_digits(1),1)
+			self.assertEqual(fact_digits(101), 3)
+			self.assertEqual(fact_digits(111), 3)
+			self.assertEqual(fact_digits(145), 145)
+			self.assertEqual(fact_digits(999), 1088640)
+			self.assertNotEqual(fact_digits(101), 2)
+			self.assertNotEqual(fact_digits(111), 1)
+			self.assertNotEqual(fact_digits(145), 144)
+			self.assertNotEqual(fact_digits(999), 999)
+
+if __name__ == '__main__':
+	unittest.main()
+
+
