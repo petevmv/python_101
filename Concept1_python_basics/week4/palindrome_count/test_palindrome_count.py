dc1688d50b3b029c68bc2eebd6d64598898023ee
Maksim Petev <petevmv@gmail.com>
2022-06-14 13:01:40 -0400

    add tests for palindromes_count

--- Concept1_python_basics/week4/palindrome_count/test_palindrome_count.py
+++ Concept1_python_basics/week4/palindrome_count/test_palindrome_count.py
@@ -1,1 +1,20 @@
 import unittest
+from palindromes_count import palindromes_count
+
+class TestPalindromeCount(unittest.TestCase):
+	def test_palindrome_count(self):
+		tests = [
+				(10, 0),
+				(20, 1),  # only 11
+				(101, 10),  # 11, 22, 33, 44, 55, 66, 77, 88, 99, 101
+				(92009, 1009),
+				(99999, 1089)
+				]
+
+		with self.subTest("Testing proper behaiviour"):
+			for actual, expected in tests:
+				self.assertEqual(palindromes_count(actual), expected)
+
+
+if __name__ == '__main__':
+	unittest.main()
