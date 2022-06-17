27a2096cc4f60a3e3526ead5df690e801b58e80c
Maksim Petev <petevmv@gmail.com>
2022-06-17 13:14:53 +0300

    Gas_station function behaive correct after refactor, added test file as well

---
+++ Concept1_python_basics/week5/Gas_stations/test_gas_station.py
@@ -0,0 +1,20 @@
+import unittest
+from Gas_Station import gas_stations
+
+class TestGasStation(unittest.TestCase):
+	def test_gas_stations(self):
+		test_cases = [
+			(gas_stations(320, 90, [50, 80, 140, 180, 220, 290]), [80, 140, 220, 290]),
+			(gas_stations(390, 80, [70, 90, 140, 210, 240, 280, 350]), [70, 140, 210, 280, 350]),
+			(gas_stations(100, 50, [10, 20, 30, 40, 50, 60, 70, 80, 90, 150]), [40, 80]),
+			(gas_stations(100, 50, [10, 90]), []),
+			(gas_stations(150, 40, [30, 40, 80, 90, 130]), [])
+		]
+
+		with self.subTest("Checing proper function behaivour"):
+			for actual, expected in test_cases:
+				self.assertEqual(actual, expected)
+
+
+if __name__ == '__main__':
+	unittest.main()
