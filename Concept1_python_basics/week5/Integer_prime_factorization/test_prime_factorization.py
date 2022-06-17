44ad40e88095920b4836c5d93b81c702ffc8cae2
Maksim Petev <petevmv@gmail.com>
2022-06-17 16:43:16 +0300

    added test for the is_it_prime function

--- Concept1_python_basics/week5/Integer_prime_factorization/test_prime_factorization.py
+++ Concept1_python_basics/week5/Integer_prime_factorization/test_prime_factorization.py
@@ -14,6 +14,16 @@ class TestPrimeFactorization(unittest.TestCase):
 		with self.subTest("Testing proper behaiviour of prime_factorization"):
 			for actual, expected in cases:
 				self.assertEqual(prime_factorization(actual), expected)
+	def test_is_it_prime(self):
+		cases_true = [2, 3, 5, 7, 13, 11, 13, 17, 19, 23, 29]
+		cases_false = [6, 8, 9, 10, 12, 14, 15, 16, 18, 20, 21, 22, 24, 25, 26, 27, 28]
+
+		with self.subTest('Testing proper behaiviour of test_is_it_prime'):
+			for actual in cases_true:
+				self.assertTrue(is_it_prime(actual))
+			for actual in cases_false:
+				self.assertFalse(is_it_prime(actual))
+
 
 if __name__ == '__main__':
 	unittest.main()
