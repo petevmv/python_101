b6037487a292126cf5581a85f406d55d6e9813f8
Maksim Petev <petevmv@gmail.com>
2022-06-21 16:41:45 -0400

    added tests for outside_of_bounds now checks for True and False

--- Concept1_python_basics/week6/stranger_form/test_stranger_form.py
+++ Concept1_python_basics/week6/stranger_form/test_stranger_form.py
@@ -8,12 +8,16 @@ class TestStrangerForm(unittest.TestCase):
 				[[1, 2, 3, 4], [5, 6, 7, 8]],
 				[[1,2],[3, 4]]
 				]		
+		
 		with self.subTest('Testing outside_of_bounds'):
 			for matrix in tests:
 				self.assertFalse(outside_of_bounds((0, 1), matrix))
 				self.assertFalse(outside_of_bounds((1, 1), matrix))
 				self.assertFalse(outside_of_bounds((1, 0), matrix))
 				self.assertFalse(outside_of_bounds((0, 0), matrix))
+				self.assertTrue(outside_of_bounds((0, 4), matrix))
+				self.assertTrue(outside_of_bounds((2, 0), matrix))
+				self.assertTrue(outside_of_bounds((2, 4), matrix))
 
 
 if __name__ == '__main__':
