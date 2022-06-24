1a772df3210b2670fcb6ad8776f1f5c5cd71248f
Maksim Petev <petevmv@gmail.com>
2022-06-24 18:05:37 -0400

    added tests for add_point

--- Concept1_python_basics/week6/stranger_form/test_stranger_form.py
+++ Concept1_python_basics/week6/stranger_form/test_stranger_form.py
@@ -6,7 +6,7 @@ class TestStrangerForm(unittest.TestCase):
 		tests = [
 				[[1, 2, 3], [4, 5, 6]],
 				[[1, 2, 3, 4], [5, 6, 7, 8]],
-				[[1,2],[3, 4]]
+				[[1, 2], [3, 4]]
 				]		
 		
 		with self.subTest('Testing outside_of_bounds'):
@@ -22,33 +22,32 @@ class TestStrangerForm(unittest.TestCase):
 	def test_build_friends_relative_position(self):
 		test = [(
 				["A", "BAA", "FRA", "CAB", "DRC", "EAD", "GLE"],
-				{
-				'A':(0, 0), 
-				'B':(-1, 0), 
-				'C':(-2, 0), 
-				'D':(-2, 1),
-				'E':(-3, 1), 
-				"F":(0, 1),
-				"G":(-3,0)
-				}
-				),
-				(
+				{'A':(0, 0), 
+				 'B':(-1, 0), 
+				 'C':(-2, 0), 
+				 'D':(-2, 1),
+				 'E':(-3, 1), 
+				 "F":(0, 1),
+				 "G":(-3,0)}
+			),
+			(
 				['A', 'BBA', "FLA", "CBB",'DLC', 'EBD','GRE'],
-				
-				{
-				'A': (0, 0),
-				'B': (1, 0),
-				'C': (2, 0),
-				'D': (2, -1),
-				'E': (3, -1),
-				'F': (0, -1),
-				'G': (3, 0)
-				}
-				)]
+				{'A': (0, 0),
+				 'B': (1, 0),
+				 'C': (2, 0),
+				 'D': (2, -1),
+				 'E': (3, -1),
+				 'F': (0, -1),
+				 'G': (3, 0)}
+			)
+		]
+		
+
 		with self.subTest('Testing build_friends_relative_position()'):
 			for configuration, expected in test:
 				self.assertEqual(build_friends_relative_position(configuration), expected)
 
+	
 	def test_in_cinema(self):
 		points_in = [(0, 0),
 				 	 (0, 1),
@@ -59,6 +58,7 @@ class TestStrangerForm(unittest.TestCase):
 					 (4, 0),
 					 (9, 3)]
 	 	
+		
 		points_out = [(0, 2),
  					  (0, 6),
 					  (0, 8),
@@ -69,6 +69,7 @@ class TestStrangerForm(unittest.TestCase):
 					  (3, 10),
 					  (9, 0)]
 
+		
 		cinema_layout = [
 						'..*...*.**',
 						'.....**...',
@@ -88,6 +89,20 @@ class TestStrangerForm(unittest.TestCase):
 				self.assertTrue(in_cinema(point, cinema_layout))
 			for point in points_out:
 				self.assertFalse(in_cinema(point, cinema_layout))
+
+
+	def test_add_point(self):
+		tests = [((0, 0), (0, 1), (0, 1)),
+				((1, 1), (0, 0), (1, 1)),
+				((1, 1), (1, 1), (2, 2)),
+				((1, 5), (3, 8), (4, 13)),
+				((-1, 3), (1, -3), (0, 0)),
+				((-1, -3), (-1, -3),(-2, -6))]
+		
+
+		with self.subTest('Testing add_point'):
+			for point1, point2, expected in tests:
+				self.assertEqual(add_point(point1, point2), expected)
 
 if __name__ == '__main__':
 	unittest.main()
