119eaa6517d73a447eb455d22f62d0ad5264e787
Maksim Petev <petevmv@gmail.com>
2022-06-25 16:17:41 -0400

    using fstring into a for loop now shows which exact test case is not satisfied

--- Concept1_python_basics/week1/Factorial_digits/test_fact_digit.py
+++ Concept1_python_basics/week1/Factorial_digits/test_fact_digit.py
@@ -3,29 +3,21 @@ from fact_digit import fact_digits
 
 class TestFactorialDigit(unittest.TestCase):
 	def test_fact_digit(self):
-		with self.subTest('Value Error'):
-			with self.assertRaises(ValueError):
-				fact_digits('abc')
-			with self.assertRaises(ValueError):
-				fact_digits([])
-			with self.assertRaises(ValueError):
-				fact_digits({})
-			with self.assertRaises(ValueError):
-				fact_digits((1, 2))
-		with self.subTest('Type Error'):
-			with self.assertRaises(TypeError):
-				fact_digits(1,2,3,4,5)
+		cases_equal = [
+					  (1, 1),
+					  (101, 3),
+					  (111, 3),
+					  (145, 145),
+					  (999, 1088640)
+			    	  ]
+		for fact_input, expected in cases_equal:
+			with self.subTest(f'Expected: {expected}'):
+				self.assertEqual(fact_digits(fact_input), expected)
 			
-		with self.subTest('Testing fact_digit() behave as expected'):
-			self.assertEqual(fact_digits(1),1)
-			self.assertEqual(fact_digits(101), 3)
-			self.assertEqual(fact_digits(111), 3)
-			self.assertEqual(fact_digits(145), 145)
-			self.assertEqual(fact_digits(999), 1088640)
-			self.assertNotEqual(fact_digits(101), 2)
-			self.assertNotEqual(fact_digits(111), 1)
-			self.assertNotEqual(fact_digits(145), 144)
-			self.assertNotEqual(fact_digits(999), 999)
+			# self.assertNotEqual(fact_digits(101), 2)
+			# self.assertNotEqual(fact_digits(111), 1)
+			# self.assertNotEqual(fact_digits(145), 144)
+			# self.assertNotEqual(fact_digits(999), 999)
 
 if __name__ == '__main__':
 	unittest.main()
