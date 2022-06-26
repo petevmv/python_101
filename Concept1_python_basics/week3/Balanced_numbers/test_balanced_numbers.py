bb8b9a0cb928e9103055e157c1329007d692239a
Maksim Petev <petevmv@gmail.com>
2022-06-26 15:51:29 -0400

    removed the TypeError raises and added refactor for better readability and testing

--- Concept1_python_basics/week3/Balanced_numbers/test_balanced_numbers.py
+++ Concept1_python_basics/week3/Balanced_numbers/test_balanced_numbers.py
@@ -3,34 +3,18 @@ from Balanced_numbers import is_number_balanced
 
 class TestBalancedNumbers(unittest.TestCase):
 	def test_is_number_balanced(self):
-		tests_true = [
-				is_number_balanced(9),
-				is_number_balanced(4518),
-				is_number_balanced(1238033) 
-				]
+		tests_true = [9, 4518, 1238033]
 
-		tests_false = [
-				is_number_balanced(28471),
-				is_number_balanced(1234567),
-				is_number_balanced(234563498573)				
-				]	 
+		tests_false = [28471, 1234567, 234563498573]	 
 		
-		with self.subTest('Testing proper function behaivour'):
-			for i in tests_true:
-				self.assertTrue(i)
-			for i in tests_false:
-				self.assertFalse(i)
+		for balanced_num in tests_true:
+			with self.subTest(f'test fails with ({balanced_num})'):
+				self.assertTrue(is_number_balanced(balanced_num))
 		
-		with self.subTest("Value Error"):
-			with self.assertRaises(ValueError):
-				is_number_balanced([])
-			with self.assertRaises(ValueError):
-				is_number_balanced({})
-			with self.assertRaises(ValueError):
-				is_number_balanced((1, 2))
-			with self.assertRaises(ValueError):
-				is_number_balanced('abc')
-			with self.assertRaises(ValueError):
-				is_number_balanced(1231.3)
+		for not_balanced_num in tests_false:
+			with self.subTest(f'test fails with ({not_balanced_num})'):
+				self.assertFalse(is_number_balanced(not_balanced_num))
+
+		
 if __name__ == '__main__':
 	unittest.main()
