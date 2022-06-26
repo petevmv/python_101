fe6292163d3cbbd981fdb58a3ff5b7ecf98db52a
Maksim Petev <petevmv@gmail.com>
2022-06-26 15:36:22 -0400

    sum_matrix now raises TypeError when called without array/matrix

--- Concept1_python_basics/week2/sum_matrix/sum_matrix.py
+++ Concept1_python_basics/week2/sum_matrix/sum_matrix.py
@@ -1,6 +1,6 @@
 import numpy as np
 def sum_matrix(m):
-    if isinstance(m, str):
-        raise TypeError('function does not accept string as param')
+    if not isinstance(m, list):
+        raise TypeError(f'sum_matrix only takes array/matrix as argument, you have given it {m}')
     arr = np.array(m)
     return arr.sum()
--- Concept1_python_basics/week2/sum_matrix/test_sum_matrix.py
+++ Concept1_python_basics/week2/sum_matrix/test_sum_matrix.py
@@ -9,17 +9,9 @@ class TestSumMatrix(unittest.TestCase):
 				  	([[0, 0, 0], [0, 0, 0], [0, 0, 0]], 0),
 				  	([[1, 2], [3, 4], [5, 6], [7, 8], [9, 10]], 55)
 				]
-		with self.subTest('Testing sum_matrix behaiviour'):
-			for actual, expected in test_eq:
+		for actual, expected in test_eq:
+			with self.subTest('Expected: {expected}'):
 				self.assertEqual(sum_matrix(actual), expected)
-		with self.subTest('Type Error'):
-			with self.assertRaises(TypeError):
-				sum_matrix()
-			with self.assertRaises(TypeError):
-				sum_matrix([1,2,3],[1,2,3])
-			with self.assertRaises(TypeError):
-				sum_matrix("abc")
-
 
 if __name__ == "__main__":
 	unittest.main()
