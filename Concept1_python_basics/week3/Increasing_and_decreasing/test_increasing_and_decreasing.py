1d7934d8a8336b6c719b489562c939ba0588417e
Maksim Petev <petevmv@gmail.com>
2022-06-26 16:30:46 -0400

    small refactor for better readability of tests and tested object

--- Concept1_python_basics/week3/Increasing_and_decreasing/test_increasing_and_decreasing.py
+++ Concept1_python_basics/week3/Increasing_and_decreasing/test_increasing_and_decreasing.py
@@ -1,22 +1,57 @@
 import unittest
 from Increasing_and_decreasing import *
 
-class TestIcreasingAndDecreasing(unittest.TestCase):
+class TestIncreasingAndDecreasing(unittest.TestCase):
 	def test_increasing_or_decresing(self):
-		tests = [([1, 2, 3, 4, 5], Monotonicity.INCREASING),
-			([5, 6, -10], Monotonicity.NONE),
-			([1, 1, 1, 1], Monotonicity.NONE),
-			([9, 8, 7, 6], Monotonicity.DECREASING),
-			([], Monotonicity.NONE),
-			([1], Monotonicity.NONE),
-			([1, 100], Monotonicity.INCREASING),
-			([1, 100, 100], Monotonicity.NONE),
-			([100, 1], Monotonicity.DECREASING),
-			([100, 1, 1], Monotonicity.NONE),
-			([100, 1, 2], Monotonicity.NONE)]
+		tests = [
+			(
+				[1, 2, 3, 4, 5], 
+				Monotonicity.INCREASING
+			),
+			(
+				[5, 6, -10], 
+				Monotonicity.NONE
+			),
+			(
+				[1, 1, 1, 1], 
+				Monotonicity.NONE
+			),
+			(
+				[9, 8, 7, 6],
+				Monotonicity.DECREASING
+		 	),
+			(
+				[],
+				Monotonicity.NONE
+			),
+			(
+				[1], 
+				Monotonicity.NONE
+			),
+			(
+				[1, 100],
+				Monotonicity.INCREASING
+			),
+			(
+				[1, 100, 100], 
+				Monotonicity.NONE
+			),
+			(
+				[100, 1], 
+				Monotonicity.DECREASING
+			),
+			(
+				[100, 1, 1], 
+				Monotonicity.NONE
+			),
+			(
+				[100, 1, 2], 
+				Monotonicity.NONE
+			)
+		]
 
-		with self.subTest('Testing proper behaivior of increasing_or_decresing'):
-			for actual, expected in tests:
+		for actual, expected in tests:
+			with self.subTest('Expected: {expected}'):
 				self.assertEqual(increasing_or_decreasing(actual), expected)
 
 if __name__ == '__main__':
