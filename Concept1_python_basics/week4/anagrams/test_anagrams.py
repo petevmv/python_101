e03de5e1f68876d2c05d467cb2655e3247208de5
Maksim Petev <petevmv@gmail.com>
2022-06-26 17:32:34 -0400

    removed TypeErrors tests and refactor test structure/list for better readability as well as added better subTest message if one fails

--- Concept1_python_basics/week4/anagrams/test_anagrams.py
+++ Concept1_python_basics/week4/anagrams/test_anagrams.py
@@ -4,31 +4,40 @@ from anagram import anagrams
 class TestAnagram(unittest.TestCase):
 	def test_anagrams(self):
 		tests_true = [
-					("listen", "silent"),
-					("LISTEN", "silent"),
-					("New York Times", "monkeys write")
-					]
+				(
+					"listen",
+					"silent"
+				),
+				(
+					"LISTEN",
+					"silent"
+				),
+				(
+					"New York Times",
+					"monkeys write"
+				)
+		]
 
 
 		tests_false = [
-					("snake", "sssnakee"),
-					("python", "ruby")
-					]
-		
-		with self.subTest('Test anagram function behaive as expected'):
-			for word1, word2 in tests_true:
+				(
+					"snake",
+					"sssnakee"
+				),
+				(
+					"python",
+					"ruby"
+				)
+		]
+
+		for word1, word2 in tests_true:
+			with self.subTest(f'{word1} and {word2} should be anagrams'):
 				self.assertTrue(anagrams(word1, word2))
-			for word1, word2 in tests_false:
-				self.assertFalse(anagrams(word1, word2))
+		
+		for word1, word2 in tests_false:
+			with self.subTest(f'{word1} and {word2} should NOT be anagrams'):
+					self.assertFalse(anagrams(word1, word2))
 
-		with self.subTest('Attribute Error'):
-			with self.assertRaises(AttributeError):
-				anagrams(1,2)
-			with self.assertRaises(AttributeError):
-				anagrams([],1)
-		with self.subTest('Type Error'):		
-			with self.assertRaises(TypeError):
-				anagrams('alabala')
-
+		
 if __name__ == '__main__':
 	unittest.main()
