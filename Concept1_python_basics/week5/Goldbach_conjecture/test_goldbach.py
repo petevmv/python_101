5f850639b1ab66192fe0fbb289e453934173fb09
Maksim Petev <petevmv@gmail.com>
2022-06-27 05:13:53 -0400

    refactor for better readability as well as added the context menager into a for loop and changed the subTest message

--- Concept1_python_basics/week5/Goldbach_conjecture/test_goldbach.py
+++ Concept1_python_basics/week5/Goldbach_conjecture/test_goldbach.py
@@ -4,16 +4,34 @@ from Goldbach import goldbach
 class TestGoldbach(unittest.TestCase):
 	def test_goldbach(self):
 		test_cases = [
-				(4, [(2,2)]),
-				(6, [(3,3)]),
-				(8, [(3,5)]),
-				(10, [(3,7), (5,5)]),
-				(100, [(3, 97), (11, 89), (17, 83), (29, 71), (41, 59), (47, 53)]),
-				(5, None)
-				]
+				(
+					4,
+					[(2,2)]
+				),
+				(
+					6, 
+					[(3,3)]
+				),
+				(
+					8, 
+					[(3,5)]
+				),
+				(
+					10, 
+					[(3,7), (5,5)]
+				),
+				(
+					100, 
+					[(3, 97), (11, 89), (17, 83), (29, 71), (41, 59), (47, 53)]
+				),
+				(
+					5,
+					None
+				)
+		]
 
-		with self.subTest('Testing proper behaiviour of goldbach function'):
-			for actual, expected in test_cases:
+		for actual, expected in test_cases:	
+			with self.subTest(f'Expected: {expected}'):
 				self.assertEqual(goldbach(actual), expected)
 
 
