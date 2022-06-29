172ca1e03d5ed2b779c59dca5d6e154736f1aa35
Maksim Petev <petevmv@gmail.com>
2022-06-29 10:41:55 -0400

    refactor the reduce_file_path using regex

--- Concept2_linux_and_python_setup/reduce_filepath/reduce_file_path.py
+++ Concept2_linux_and_python_setup/reduce_filepath/reduce_file_path.py
@@ -1,16 +1,42 @@
-from collections import deque
+import re
+# from collections import deque
 
 def reduce_file_path(path):
-    result = deque()
-    for ch in path.split('/'):
-        if ch == '..' and len(result) > 0:
-            result.pop()
-        if ch not in ('.','..',''):
-            result.append(ch)
+    patterns = [
+            (
+                '.\w*.[.]{2,}',  # pattern matches dots (someword/../) and the word the its left
+                ''
+            ),
+            (
+                '[.]{1,}', # pattern matches single dot /././. 
+                ''
+            ),
+            (
+                '[\/]{2,}', # pattern matches all remaining back slashes //, its used last  
+                '/'         # as the pattern above sometimes leaves more slashes when used
+            )
+
+    ]
+
+    for pattern, substitution in patterns:
+        path = re.sub(pattern, substitution, path)
+
+    while path[-1] in ('/') and len(path) > 1:
+        path = path[0:-1]
+
+    return ''.join(path)
 
 
-    print('/' + '/'.join(result))
-    return '/' + '/'.join(result)
+    # result = deque()
+    # for ch in path.split('/'):
+    #     if ch == '..' and len(result) > 0:
+    #         result.pop()
+    #     if ch not in ('.','..',''):
+    #         result.append(ch)
+
+
+    # print('/' + '/'.join(result))
+    # return '/' + '/'.join(result)
 
 
 
